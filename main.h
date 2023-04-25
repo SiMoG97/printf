4385370e206ed1d621cea007a17dd7764505599a
SiMoG97 <echaaranimohamed@gmail.com>
2023-04-25 02:44:01 -0700

    adding README.md and main.h files

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_d(int num);
+int print_s(char *str);
+
+#endif
