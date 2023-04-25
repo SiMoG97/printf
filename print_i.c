20bcf30b977edaa0e9db22d9039424aa77e1cc5d
amin0991 <86832.belhajji@students.itu.edu>
2023-04-25 14:10:37 -0700

    1 task

--- print_i.c
+++ print_i.c
@@ -61,6 +61,25 @@ int print_X(va_list X)
  *
  * Return: number of characters to print
  */
+/**
+ * _pow - calculates an exponent
+ * @base: base of exponent
+ * @exponent: exponent of number
+ *
+ * Return: base ^ exponent
+ */
+static unsigned long _pow(unsigned int base, unsigned int exponent)
+{
+	unsigned int i;
+	unsigned long ans = base;
+
+	for (i = 1; i < exponent; i++)
+	{
+		ans *= base;
+	}
+	return (ans);
+}
+
 int print_p(va_list p)
 {
 	int count = 0;
