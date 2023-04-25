50a69f41253f44d07978d2af38d1427414fd55ce
amin0991 <86832.belhajji@students.itu.edu>
2023-04-25 14:29:02 -0700

    1 task

--- main.h
+++ main.h
@@ -13,7 +13,6 @@ int print_handler(const char *format, va_list args);
 int print_hex(unsigned int n, unsigned int c);
 int print_x(va_list x);
 int print_X(va_list X);
-static unsigned long _pow(unsigned int base, unsigned int exponent);
 int print_p(va_list p);
 
 #endif
